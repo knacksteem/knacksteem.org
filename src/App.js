ed519e2e70d5aa460942626ef16c1bdf6a59a483
Nikitas Christodoulakis <me@bannik.eu>
2018-06-01 14:04:01 +0300

    oath config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import sc2 from 'sc2-sdk';
 
 class App extends Component {
+  getOathURL(){
+    let api = sc2.Initialize({
+        app: 'knacksteem.app',
+        callbackURL: 'http://localhost:3000/callback',
+        scope: ['login', 'custom_json', 'claim_reward_balance', 'vote', 'comment']
+    });
+    return api.getLoginURL();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <a href={this.getOathURL()}>Login</a>
       </div>
     );
   }
