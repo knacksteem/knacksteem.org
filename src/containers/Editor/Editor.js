e04ada9122e23ab9256f917b652734a47a65e1c9
devamaz <ahmad.abdulaziz37@gmail.com>
2018-06-01 22:06:06 +0100

    minor edit

---
+++ src/containers/Editor/Editor.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Editor.scss';
+
+let mark = require('marked');
+ state=
+
+
+class Editor extends Component {
+  render() {
+    return (
+        <div>
+          <form>
+
+          </form>
+        </div>
+    );
+  }
+}
+
+export default Editor;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,6 +4,7 @@ import sc2 from 'sc2-sdk';
 
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Editor from '../Editor/Editor';
 
 class Home extends Component {
   getOathURL(){
@@ -14,12 +15,16 @@ class Home extends Component {
     });
     return api.getLoginURL();
   }
+
   render() {
     return (
       <div className="App">
+
         <Header/>
         <a href={this.getOathURL()}>Login</a>
+        <Editor/>
         <Footer/>
+
       </div>
     );
   }
