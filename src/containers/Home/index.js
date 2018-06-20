9aa3bc49dc4335a3fb2e66271375d7bce283f5ba
Andreas Teufel <a.teufel@limesoda.com>
2018-06-20 17:26:46 +0200

    base layout for articles with antd list component

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
 import './index.css';
 import sc2 from 'sc2-sdk';
-import { Layout, Input } from 'antd';
+import { Layout, Input, List } from 'antd';
 import Article from '../../components/Article';
 const { Header, Content } = Layout;
 const Search = Input.Search;
+
+const listData = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'http://ant.design',
+    title: `ant design part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 class Home extends Component {
   constructor({history, location}) {
@@ -42,7 +53,12 @@ class Home extends Component {
         </Header>
         <Content>
           {/*{this.props.location.pathname}*/}
-          <Article />
+          <List
+            itemLayout="vertical"
+            size="large"
+            dataSource={listData}
+            renderItem={data => <Article data={data} />}
+          />
         </Content>
         {/*<a href={this.getOathURL()}>Login</a>*/}
       </div>
