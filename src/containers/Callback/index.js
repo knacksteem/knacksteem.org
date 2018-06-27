f6bebe4687dfe6de606c4a3180b9110837b587d1
ateufel <luschn@gmx.at>
2018-06-28 00:06:10 +0200

    micro-fix: add base content layout to callback route

--- src/containers/Callback/index.js
+++ src/containers/Callback/index.js
@@ -3,8 +3,10 @@ import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {push} from 'react-router-redux';
 import PropTypes from 'prop-types';
+import {Layout} from 'antd';
 import qs from 'qs';
 import {userLogin} from '../../actions/user';
+const {Content} = Layout;
 
 //Callback route for Steem Connect
 class Callback extends Component {
@@ -33,7 +35,9 @@ class Callback extends Component {
   render() {
     return (
       <div>
-        Logging in...
+        <Content>
+          Logging in...
+        </Content>
       </div>
     );
   }
