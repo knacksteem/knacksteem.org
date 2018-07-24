bae40817c227b0977d1428e129876abbe44dc8d7
ateufel <luschn@gmx.at>
2018-07-24 21:55:04 +0200

    add popconfirm dialog before delete. add loader animation on upvote and delete operations.

--- src/components/Common/ArticleMetaBottom.js
+++ src/components/Common/ArticleMetaBottom.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {Divider} from 'antd';
+import {Divider, Popconfirm, Spin} from 'antd';
 import IconText from '../Common/IconText';
 import {prettyDate} from '../../services/functions';
 import {upvoteElement, deleteElement} from '../../actions/articles';
@@ -19,63 +19,86 @@ import Cookies from 'js-cookie';
  * @param onEditClick callback function when the user clicks on the edit link
  * @param onReplyClick callback function when the user clicks on the reply link
  */
-const ArticleMetaBottom = ({data, onUpdate, dispatch, isComment, isArticleDetail, isEditMode, onEditClick, onReplyClick}) => {
-  const onUpvoteClick = async () => {
+class ArticleMetaBottom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isBusy: false
+    };
+  }
+  //upvote article or comment
+  onUpvoteClick = async () => {
+    const {data, dispatch, onUpdate} = this.props;
+
     //if already voted, immediately return - maybe implement unvoting later, if needed
     if (data.isVoted) {
       return;
     }
     //upvote with 10000 - which equals 100%
     try {
+      this.setState({isBusy: true});
       await dispatch(upvoteElement(data.author, data.permlink, 10000));
+      this.setState({isBusy: false});
       //on successful update, reload article or article list
       onUpdate();
     } catch (err) {
       console.log(err);
     }
   };
-  const onDeleteClick = async () => {
+  //delete article or comment - will get called after confirmation
+  onDeleteClick = async () => {
+    const {data, dispatch, onUpdate} = this.props;
+
     try {
+      this.setState({isBusy: true});
       await dispatch(deleteElement(data.permlink));
+      this.setState({isBusy: false});
       //on successful update, reload article or article list
       onUpdate();
     } catch (err) {
       console.log(err);
     }
   };
+  render() {
+    const {isBusy} = this.state;
+    const {data, isComment, isArticleDetail, onEditClick, onReplyClick, isEditMode} = this.props;
 
-  const isAuthor = (Cookies.get('username') === data.author);
-  const commentCount = isComment ? data.replies.length : data.commentsCount;
+    const isAuthor = (Cookies.get('username') === data.author);
+    const commentCount = isComment ? data.replies.length : data.commentsCount;
 
-  const actionsArray = [<a key="action-reply" onClick={onReplyClick}>Reply</a>];
-  if (isComment || isArticleDetail) {
-    if (isAuthor) {
-      actionsArray.push(
-        <a key="action-edit" onClick={onEditClick}>Edit</a>
-      );
+    const actionsArray = [<a key="action-reply" onClick={onReplyClick}>Reply</a>];
+    if (isComment || isArticleDetail) {
+      if (isAuthor) {
+        actionsArray.push(
+          <a key="action-edit" onClick={onEditClick}>Edit</a>
+        );
+      }
+      if (isAuthor && !commentCount && !data.votesCount) {
+        actionsArray.push(
+          <Popconfirm title="Are you sure?" onConfirm={this.onDeleteClick} key="action-delete">
+            <a>Delete</a>
+          </Popconfirm>
+        );
+      }
     }
-    if (isAuthor && !commentCount && !data.votesCount) {
-      actionsArray.push(
-        <a key="action-delete" onClick={onDeleteClick}>Delete</a>
-      );
-    }
+
+    return (
+      <div>
+        <IconText type="clock-circle-o" text={prettyDate(data.postedAt)} />
+        <Divider type="vertical" />
+        <IconText type="message" text={commentCount} />
+        <Divider type="vertical" />
+        <span className={`upvote ${data.isVoted ? 'active' : ''}`} onClick={this.onUpvoteClick}><IconText type={data.isVoted ? 'up-circle' : 'up-circle-o'} text={data.votesCount} /></span>
+        <Divider type="vertical" />
+        <IconText type="wallet" text={`$${data.totalPayout}`} />
+        <Divider type="vertical" />
+        <span className="action-links">
+          {(!isEditMode && !isBusy) && actionsArray}
+          {isBusy && <Spin size="small" />}
+        </span>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <IconText type="clock-circle-o" text={prettyDate(data.postedAt)} />
-      <Divider type="vertical" />
-      <IconText type="message" text={commentCount} />
-      <Divider type="vertical" />
-      <span className={`upvote ${data.isVoted ? 'active' : ''}`} onClick={onUpvoteClick}><IconText type={data.isVoted ? 'up-circle' : 'up-circle-o'} text={data.votesCount} /></span>
-      <Divider type="vertical" />
-      <IconText type="wallet" text={`$${data.totalPayout}`} />
-      <Divider type="vertical" />
-      <span className="action-links">
-        {!isEditMode && actionsArray}
-      </span>
-    </div>
-  );
 };
 
 ArticleMetaBottom.propTypes = {
