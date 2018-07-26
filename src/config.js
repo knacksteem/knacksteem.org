7cef9c64c4c43f6668a85e12be3ffbfe1ada8e91
ateufel <luschn@gmx.at>
2018-07-26 22:24:10 +0200

    remove unused string in config file.

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ export default {
   SteemConnect: {
     scope: ['login', 'custom_json', 'claim_reward_balance', 'vote', 'comment', 'delete_comment'],
     callbackURL: `${window.location.origin}/callback`
-  },
-  GeneralError: 'error occured, please contact an admin or try again later'
+  }
 };
