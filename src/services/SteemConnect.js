43a748f73338efc2ebab314d8554e3ed9ff958dd
ateufel <luschn@gmx.at>
2018-07-26 22:25:47 +0200

    add comment for steem connect singleton.

--- src/services/SteemConnect.js
+++ src/services/SteemConnect.js
@@ -1,6 +1,9 @@
 import sc2 from 'sc2-sdk';
 import Config from '../config';
 
+/**
+ * steem connect singleton
+ */
 const api = sc2.Initialize({
   app: 'knacksteem.app',
   callbackURL: Config.SteemConnect.callbackURL,
