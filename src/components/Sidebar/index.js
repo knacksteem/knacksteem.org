ef9a2fe9b3fc41e50fc54aa72e0ef8392c116e0b
Brooks Boyd <bro@oks.me>
2018-09-10 23:56:26 -0500

    Add Font Awesome and use icons for menu items

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,7 +11,7 @@ const CustomSidebar = ({location, user, articles}) => {
   let links = [];
   if (user.username && user.isContributor) {
     links.push(
-      <Menu.Item key="/mycontributions"><Link to="/mycontributions">My Contributions</Link></Menu.Item>
+      <Menu.Item key="/mycontributions"><Link to="/mycontributions"><i className="fas fa-pen"></i>My Contributions</Link></Menu.Item>
     );
     links.push(
       <Menu.Item key="user-divider"><Divider/></Menu.Item>
@@ -19,13 +19,13 @@ const CustomSidebar = ({location, user, articles}) => {
   }
   if (user.isModerator || process.env.NODE_ENV === 'development') {
     links.push(
-      <Menu.Item key="/moderation/pending"><Link to="/moderation/pending">Pending</Link></Menu.Item>
+      <Menu.Item key="/moderation/pending"><Link to="/moderation/pending"><i className="fas fa-bars"></i>Pending</Link></Menu.Item>
     );
     links.push(
-      <Menu.Item key="/moderation/reserved"><Link to="/moderation/reserved">Reserved</Link></Menu.Item>
+      <Menu.Item key="/moderation/reserved"><Link to="/moderation/reserved"><i className="fas fa-bookmark"></i>Reserved</Link></Menu.Item>
     );
     links.push(
-      <Menu.Item key="/users"><Link to="/users">Users</Link></Menu.Item>
+      <Menu.Item key="/users"><Link to="/users"><i className="fas fa-users"></i>Users</Link></Menu.Item>
     );
     links.push(
       <Menu.Item key="mod_divider"><Divider/></Menu.Item>
@@ -35,6 +35,16 @@ const CustomSidebar = ({location, user, articles}) => {
     <Sider width={200}>
       <Menu mode="inline" selectedKeys={[location.pathname]} style={{height: '100%', borderRight: 0, marginTop: '20px'}}>
         {links}
+        <Menu.Item key="/guidelines">
+          <Link to="/guidelines"><i className="fas fa-th"></i>Guidelines</Link>
+        </Menu.Item>
+        <Menu.Item key="/faq">
+          <Link to="/faq"><i className="fas fa-question-circle"></i>FAQ</Link>
+        </Menu.Item>
+        <Menu.Item key="/tos">
+          <Link to="/tos"><i className="fas fa-file-contract"></i>ToS</Link>
+        </Menu.Item>
+        <Menu.Item key="mod_divider"><Divider/></Menu.Item>
         <Menu.Item key="/">
           <Link to="/">All</Link>
         </Menu.Item>
