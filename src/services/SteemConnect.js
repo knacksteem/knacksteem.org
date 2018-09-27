256a6097d72d9bc792620fc215c0d216c7b5b26f
Knowledges <31616620+onozasi@users.noreply.github.com>
2018-09-27 07:01:12 +0100

    Update SteemConnect.js

--- src/services/SteemConnect.js
+++ src/services/SteemConnect.js
@@ -5,6 +5,7 @@ import Config from '../config';
  * steem connect singleton
  */
 const api = sc2.Initialize({
+  baseURL: 'https://steemconnect.com',
   app: 'knacksteem.app',
   callbackURL: Config.SteemConnect.callbackURL,
   scope: Config.SteemConnect.scope
