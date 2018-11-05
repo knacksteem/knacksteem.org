2d84e5894dd53b42d058bf2c7dbd52eab490a696
snwolak <snwolak@outlook.com>
2018-11-05 14:18:00 +0100

    change getting delegations data
    - component is now getting data from api endpoint

--- src/containers/Sponsors/index.js
+++ src/containers/Sponsors/index.js
@@ -1,60 +1,27 @@
 import React, { Component } from 'react';
 import './index.css';
 import steem from 'steem';
+import axios from 'axios';
 import Sponsor from '../../components/Sponsor/';
+
 export default class Sponsors extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      temp: [],
       sponsors: []
     };
   }
   async componentDidMount() {
+    let json = [];
     let globalData = [];
-    let arr = [];
-    //getting history of an account TODO: Get whole history to get every delegator.
-    await this.getData();
-    //filtering to get only delegations
-    const filtered = this.state.temp.filter(op => {
-      return op[1].op[0] === 'delegate_vesting_shares';
-    });
-    filtered.map(sponsor => {
-      let obj = {
-        delegator: sponsor[1].op[1].delegator,
-        vesting_shares: Number(sponsor[1].op[1].vesting_shares.split(' ')[0]),
-        block: sponsor[1].block,
-        trx_id: sponsor[1].trx_id
-      };
-      //checking if delegator already exists in array
-      const check = arr.find(x => x.delegator === obj.delegator);
-      if(check !== undefined) {
-
-        if(check.block < sponsor[1].block) {
-          //if delegation is newer it will replace the old one
-          const filtered = arr.filter(item => {
-            return item.trx_id !== check.trx_id;
-          });
-          arr = filtered;
-          arr.push(obj);
-        }
-      } else {
-        arr.push(obj);
-      }
-
-    });
-    //deleting delegations with zero vests
-    const filterZeroVesting = arr.filter(item => {
-      return item.vesting_shares !== 0;
+    //Put your api endpoint here
+    axios.get('./delegations.json').then(res => {
+      json = res.data;
+      return res;
+    }).catch(err => {
+      console.log(err);
     });
 
-    const sorted = filterZeroVesting.sort(function(a, b){
-      const x = a.vesting_shares;
-      const y = b.vesting_shares;
-      if (x < y) {return 1;}
-      if (x > y) {return -1;}
-      return 0;
-    });
     //Getting global blockchain data to convert vests to STEEM
     await steem.api.getDynamicGlobalPropertiesAsync().then(result => {
       globalData.push(result);
@@ -63,46 +30,11 @@ export default class Sponsors extends Component {
       return err;
     });
     this.setState({
-      sponsors: sorted,
+      sponsors: json,
       globalData: globalData
     });
   }
-  async getData() {
-    let data;
-    //Number of the operation, it has to be the same or higher than limit
-    let from = this.state.temp === undefined || this.state.temp.length === 0 ? 10000000 : this.state.temp[this.state.temp.length -1 ][0];
-    await steem.api.getAccountHistoryAsync('knacksteem.org', from, from < 1000 ? from : 1000).then(res => {
-      const sorted = res.sort(function(a, b){
-        const x = a[1].block;
-        const y = b[1].block;
-        if (x < y) {return 1;}
-        if (x > y) {return -1;}
-        return 0;
-      });
-      data = sorted;
-      return res;
-    }).catch(err => {
-      return err;
-    });
-    if(this.state.temp.length === 0) {
-      this.setState({
-        temp: data
-      });
-      await this.getData();
-      return null;
-      //if pulled data is the same function stops here
-    } else if(data[data.length - 1][1].block === this.state.temp[this.state.temp.length -1][1].block) {
-      return null;
-    } else {
-      const newState = this.state.temp.concat(data);
-      this.setState({
-        temp: newState
-      });
-      await this.getData();
-      return null;
-    }
 
-  }
   render() {
     return (
       <div className="sponsors-container">
