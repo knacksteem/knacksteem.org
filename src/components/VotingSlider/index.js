0dc163cacc3eb080cadc549871a12209ec452edc
snwolak <snwolak@outlook.com>
2018-11-20 12:20:47 +0100

    del getVoteWorth and use existing function

--- src/components/VotingSlider/index.js
+++ src/components/VotingSlider/index.js
@@ -3,7 +3,14 @@ import './index.css';
 import store from '../../store';
 import {votePowerChange} from '../../actions/votingSlider';
 import { Slider, Icon } from 'antd';
-import getVoteWorth from './getVoteWorth';
+import { calculateVoteValue } from '../../services/functions';
+import delay from './delay';
+import {
+  getRewardFund,
+  getCurrentMedianHistoryPrice,
+  getDynamicGlobalProperties,
+} from '../../actions/stats';
+
 const marks = {
   0: '0%',
   25: '25%',
@@ -23,17 +30,53 @@ export default class VotingSlider extends Component {
 
   }
   async componentDidMount() {
+    Promise.all([store.dispatch(getRewardFund()),
+      store.dispatch(getCurrentMedianHistoryPrice()),
+      store.dispatch(getDynamicGlobalProperties())]).then(res => {
+      return res;
+    });
+
+    if(store.getState().user.userObjectSteemit.account === undefined) {
+      await delay(5000);
+    }
+    const state = store.getState();
+    const voteValue = calculateVoteValue({
+      votingPower: state.user.userObjectSteemit.account.voting_power * (this.state.votePower / 10000),
+      lastVoteTime: state.user.userObjectSteemit.account.last_vote_time,
+      rewardBalance: state.stats.rewardFundObject.reward_balance,
+      recentClaims: state.stats.rewardFundObject.recent_claims,
+      currentMedianHistoryPrice: state.stats.currentMedianHistoryPriceObject,
+      vestingShares: state.user.userObjectSteemit.account.vesting_shares,
+      receivedVestingShares: state.user.remoteUserObject.received_vesting_shares,
+      delegatedVestingShares: state.user.remoteUserObject.delegated_vesting_shares,
+      totalVestingFundSteem: state.stats.dynamicGlobalPropertiesObject.total_vesting_fund_steem,
+      totalVestingShares: state.stats.dynamicGlobalPropertiesObject.total_vesting_shares
+    });
+
     this.setState({
-      voteWorth: await getVoteWorth()
+      voteWorth: voteValue
     });
 
   }
   handleChange = async (e) => {
     const value = Number(e);
     store.dispatch(votePowerChange(value * 100));
+    const state = store.getState();
+    const voteValue = calculateVoteValue({
+      votingPower: state.user.userObjectSteemit.account.voting_power * (value * 100 / 10000),
+      lastVoteTime: state.user.userObjectSteemit.account.last_vote_time,
+      rewardBalance: state.stats.rewardFundObject.reward_balance,
+      recentClaims: state.stats.rewardFundObject.recent_claims,
+      currentMedianHistoryPrice: state.stats.currentMedianHistoryPriceObject,
+      vestingShares: state.user.userObjectSteemit.account.vesting_shares,
+      receivedVestingShares: state.user.remoteUserObject.received_vesting_shares,
+      delegatedVestingShares: state.user.remoteUserObject.delegated_vesting_shares,
+      totalVestingFundSteem: state.stats.dynamicGlobalPropertiesObject.total_vesting_fund_steem,
+      totalVestingShares: state.stats.dynamicGlobalPropertiesObject.total_vesting_shares
+    });
     this.setState({
       votePower: value,
-      voteWorth: await getVoteWorth()
+      voteWorth: voteValue
     });
   }
   render() {
