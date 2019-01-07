ab92c23c96b78e92efd9d15c816749923cabf79b
Emmanuel <ogbiyoyosky@yahoo.com>
2019-01-07 16:20:05 +0000

    Add privacy pocily doc

---
+++ src/containers/Privacy/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Layout} from 'antd';
+import './index.css';
+const {Content} = Layout;
+
+const Privacy = () => {
+  return (
+    <div className='privacy-container'>
+      <Content>
+              <h2>Knacksteem Privacy Policy.</h2> 
+
+                <p>Knacksteem is developed as an open source project. Our services are provided at no cost and are intended for use as is.
+                This document is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decides to use our services.
+                Further utilization of our services after reading this policy statement implies that you accept, and adhere, to all our terms and conditions. This privacy policy is applicable only to our project. The developer would not be held accountable or responsible for the content or privacy practices of any third party service provider not directly operated by the developer to which our platform links, or that links to our platform. Therefore, it is advisable to read the privacy policies of third party services that may be linked to our services. </p>
+            <hr></hr>
+
+            <h2>Information Collection and Use</h2>
+
+            <p>We offer great user experience while using our Services, and we may require you to provide us with certain personally identifiable information. This information is used to gain access to our platform. The information that we request will be retained on your device and is not collected by us in any way.</p>
+
+            <h2>Cookies</h2>
+
+            <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory. At the moment, utilizing our platform doesn't store cookies on your browser. However, this may change as the platform progresses.</p>
+
+            <h2>Service Providers</h2>
+
+            <p>We employ third-party companies and individuals due to the following reasons:</p>
+                <ul>
+                    <li>To facilitate our Service;</li>
+                    <li>To provide the Service on our behalf;</li>
+                    <li>To perform Service-related services.</li>
+                </ul>
+                <li>We want to inform you that, this third-party service provider does not have access to your Personal Information. If for any reason, their service is to ensure your personal information, which in this case, is your login information, is safe, and secure.</li>
+
+                <h2>Security</h2>
+
+                <p>We value your trust in providing your Personal Information to access our platform, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+
+                <h2>Links to Other Sites</h2> 
+
+                <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+
+                <h1>Children’s Privacy</h1>
+
+                <p>Our Services address anyone under the age of 13. This is line with the privacy policy of Steem, under which our platform is built on.</p>
+
+                <h1>Changes to This Privacy Policy</h1> 
+
+                <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this document. These changes are effective immediately after they are posted on this document.</p>
+
+                <h2>Contact Us</h2> 
+
+                <p> If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>
+
+      </Content>
+    </div>
+    );
+};
+export default Privacy;
