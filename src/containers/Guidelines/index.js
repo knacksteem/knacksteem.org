edb08033c21a474e36f03c45e04d4aa0b9585942
codingdefined <codingdefined@gmail.com>
2019-01-17 12:11:13 +0530

    Added Guidelines Text

--- src/containers/Guidelines/index.js
+++ src/containers/Guidelines/index.js
@@ -5,10 +5,146 @@ const {Content} = Layout;
 
 const Guidelines = () => {
   return (
-    <div>
+    <div className='guidelines-container'>
       <Content>
-        <h1>Guidelines</h1>
-        <p>Coming soon!</p>
+        <h1>KnackSteem Guidelines and policies</h1>
+        <hr></hr>
+        <h2>Policies</h2>
+        <hr></hr>
+        <p>Policies are rules which you must adhere to. Failure to follow any of the following policies will lead to rejection of your contribution and may lead to a ban.</p>
+        <ul>
+          <li>Only posts written in clear and easily understood English will be accepted.</li>
+          <li>Contributions and comments must not include offensive speech directed at others.</li>
+          <li>Contributions and comments may not contain false information about another user that may be perceived as defamatory.</li>
+          <li>Post may not include licensed or commercial materials, and should cite the source of Creative Commons media.</li>
+          <li>Plagiarism.</li>
+          <li>Using multiple accounts to abuse the system.</li> 
+        </ul>
+
+        <h2>Category guidelines (Tips for submitting a great post)</h2>
+        <hr></hr>
+        <p>Please read the following category guidelines carefully before writing and publishing your content on knacksteem. This will help you to be more productive.</p>
+
+        <h3>Tech Trends</h3>
+        <hr></hr>
+        <p>Tech-trends, as a skill, is being aware and mindful of newly launched technological or digital products. It also includes the ability to recognize and understand the usefulness of these advancements.</p>
+        <p>Software reviews posted on our site can help guide millions of software shoppers to select the application that best meets their needs. You should consider these tips when writing your review:</p>
+        
+        <ul>
+          <li>Be specific. What features do you love? Don’t just say you like the product. Explain why you like it. Use clear examples of specific experiences - the more detail, the better.</li>
+          <li>Be readable. Reviews need to be readable for others. Use proper grammar, without excessive capitalization or punctuation, and be sure to check your spelling.</li>
+          <li>Be objective. Try to highlight both positive and negative aspects in your review, even if your experience skews heavily in one direction.</li>
+          <li>Be current. The best reviews are those written about recent software versions and within a year of use.</li>
+          <li>Be relevant. We provide an open forum, but please keep your review useful to other software consumers. Avoid off-topic discussions or personal opinions not relevant to your direct experience using the software.</li>
+        </ul>
+
+        <h3>Gaming</h3>
+        <hr></hr>
+        <p>Computer games or video games are an interactive digital medium consisting of several different genres of digital games. This could be done through consoles, computers, mobile phone or any other gaming medium. Gaming is a term which suggests regular gameplay which is usually played for fun or as a hobby. Gaming becomes even more interesting and fascinating when you write reviews about them.</p>
+        
+        <ul>
+          <li>Play the game.</li>
+          <li>It is vital that you have a strong knowledge of the game.</li>
+          <li>While playing the game, try to take notes of what you like and what you don’t.</li>
+          <li>When you want to pen your experience, start with an introduction. Make it descriptive, possibly with a little bit of humor so it catches a reader’s attention.</li>
+          <li>Include the short notes you took while playing the game after the introduction.</li>
+          <li>Try to give details about every aspect of the game: gameplay, cinematics, story, music, graphics etc.</li>
+          <li>Always end your post with a summary of your review. Go on to share your pros and cons and maybe even rate the game.</li>
+          <li>Give your audience a view of your gaming skills. Try to include a video of you while playing the game.</li>
+        </ul>
+
+        <h3>Documentary</h3>
+        <hr></hr>
+        <p>A documentary is an act of sharing reports based on personal research and findings on a particular subject. It could be a movie, a story, facts that are real or even an idea that could have a real-world impact.</p>
+        
+        <ul>
+          <li>Start with an introduction. It might be brief or lengthy, but try to let your audience know the content of the documentary early on.</li>
+          <li>Let your audience know the purpose of doing the documentary. Whether it is to unravel a mystery about a place, people, or something about nature.</li>
+          <li>Media contents are essential components of a documentary. This means images and videos are vital components of your documentation. They make your post more interesting to read.</li>
+          <li>Share your experience during the course of the research and try to give helpful tips. Be interactive.</li>
+          <li>Be sure to make your audience curious.</li>
+          <li>Summarize your document.</li>
+        </ul>
+
+        <h3>Altruism</h3>
+        <hr></hr>
+        <p>Altruism is the act of being altruistic towards others or nature. To be concerned by a situation in which others find themselves, and go further to render some kind of help or advice.</p>
+        
+        <ul>
+          <li>Introduction. Make your audience understand what they should expect in the content.</li>
+          <li>Try to be motivating by using kind and altruistic terms.</li>
+          <li>Be personal and say what inspired you to carry out this altruistic mission. Your audience would love to know the story behind it.</li>
+          <li>Visual material such as images and videos are usually helpful and makes your content even more interesting to read.</li>
+          <li>Try to remain neutral in all context and try not to defarm others.</li>
+        </ul>
+        <p>[altruism, by its nature, is not neutral]</p>
+
+        <h3>Jokes and Humour</h3>
+        <hr></hr>
+        <p>This is the ability to be able to amuse or surprise your audience by creating a connection between different clauses which could be expressed verbally or in written form.</p>
+        
+        <ul>
+          <li>Be funny! Try not to be predictable.</li>
+          <li>Consider interesting joke materials to make your joke fun and interesting to read.</li>
+          <li>Try to be simple, unique and be considerate of your audience.</li>
+          <li>Consider your joke structure. Be consistent.</li>
+          <li>Be confident, relax, and improvise if you have to.</li>
+          <li>Include visual expression (comics, facial gesture) or image/video to improve your joke. This helps your audience understand your joke even more.</li>
+          <li>Try to avoid controversial topics that might offend someone.</li>
+        </ul>
+
+        <h3>Art</h3>
+        <hr></hr>
+        <p>Art is a graphical representation that can expresses one’s feelings or important ideas. This could be in the form of a painting, sculpture, drawing and so on. The following guidelines will help you submit a very constructive and interesting post about your artwork.</p>
+        
+        <ul>
+          <li>Start with an introduction. Let your audience know what your work is about before the main body.</li>
+          <li>Describe your artwork as if you were talking to someone who couldn’t see it.</li>
+          <li>Describe the mood of your artwork as well as the visual characteristics.</li>
+          <li>Try to mention the colors, but make sure you use words that describe the colors and their effect.</li>
+          <li>Put yourself in the mindset of the average person.</li>
+          <li>Try not to pad your post, stay on point and make it very interesting to read.</li>
+        </ul>
+
+        <h3>Music/Song</h3>
+        <hr></hr>
+        <p>Music or Song is a way of expressing one’s innermost desire through musical tunes or poems that have meanings. It usually a source of inspiration to many that make them feel alive, happy, fun, and even free. While music can mean different things to different people, this is the category to express them and show what you’ve got.</p>
+        
+        <ul>
+          <li>If you want to pen down your music, consider your writing style. It should be well formatted and consistent.</li>
+          <li>Tell your audience the inspirations behind the music’s creation.</li>
+          <li>Be real and unique all through your context.</li>
+          <li>Audio files and videos will make your blog look even more presentable.</li>
+          <li>Give it your best shot and make your audience feel the vibe and tunes.</li>
+        </ul>
+
+        <h3>DIY</h3>
+        <hr></hr>
+        <p>DIY is the ability to create, decorate, build, and even repair broken stuff all by yourself, rather than employing a professional to do it. However, this section favors both professionals and non-professionals.</p>
+        
+        <ul>
+          <li>Start with an introduction. Let your audience know what your post is about.</li>
+          <li>Be original and unique: Only share materials such as pictures or videos that you made.</li>
+          <li>Be very descriptive and show that you made it.</li>
+          <li>Try to make it personal and editorial by providing helpful tips.</li>
+          <li>Make your post fun to read.</li>
+          <li>Consider your audience, and do not use abusive words. Be respectful and kind all through.</li>
+        </ul>
+
+        <h3>Fashion</h3>
+        <hr></hr>
+        <p>Fashion is all about appearance. It can be an expression of art, nature, personality, and so much more. This is one of many ways to express one’s true freedom. In this section, you should describe your area of professional expertise as a designer, stylist, tailor, or an illustrator.</p>
+        
+        <ul>
+          <li>Be creative.</li>
+          <li>Start with a pleasant topic. This will give your readers/audience a better impression before reading.</li>
+          <li>For fashions that are related to clothing styles, make your publication as sophisticated as possible.</li>
+          <li>We recommend good visual content that will be catchy. It is critical to provide a lot of pictorial or visual content in this section.</li>
+          <li>List the materials used if you are a fashion designer or illustrator.</li>
+          <li>Include personal thoughts, and the reasons behind your choice of design.</li>
+          <li>Provide helpful tips and make your presentation very lucrative..</li>
+        </ul>
+        
       </Content>
     </div>
   );
