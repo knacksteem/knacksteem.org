4aac6989400109bd559b876ddc3435240abe4bc0
Knowledges <31616620+onozasi@users.noreply.github.com>
2019-01-20 03:15:42 +0100

    This PR updates all the categories (#86)
    * Category update
    
    * Category error call back update
    
    * Update category on header.

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -5,11 +5,16 @@ const Option = Select.Option;
 export const KnackSelect = () => {
   return  (
     <Select defaultValue="Categories" style={{ width: 120 }}>
-      <Option value="Graphic">Graphic</Option>
-      <Option value="Vlog">Vlog</Option>
-      <Option value="Art" >Art</Option>
-      <Option value="Tech Trend">Tech Trend</Option>
-      <Option value="Knack">Knack</Option>
+	  <Option value="All">All</Option>
+    <Option value="gaming">Gaming</Option>
+    <Option value="documentary">Documentary</Option>
+    <Option value="art" >Art</Option>
+    <Option value="altruism">Altruism</Option>
+    <Option value="techtrends">Tech trends</Option>
+	  <Option value="humour">Humour</Option>
+	  <Option value="music">Music</Option>
+	  <Option value="diy">DIY</Option>
+	  <Option value="fashion">Fashion</Option>
     </Select>
   ); 
 };
